0f5571d034ab2026baae7310479aabf199d4b893
hos4dbrandon <hosfordbrandon@utexas.edu>
2021-07-29 15:35:53 -0500

    Update FaceAligner.py
    Progress bar?

--- FaceAligner.py
+++ FaceAligner.py
@@ -12,16 +12,9 @@ parser.add_argument('--img_name', help='Name of test image')
 parser.add_argument('--make_video', required=True, help='Do you want to convert finished images into a video? T or F')
 args = parser.parse_args()
 
-#creating face_cascade and eye_cascade objects
-#face_cascade = cv2.CascadeClassifier('C:\\Users\\hosfo\\Documents\\GitHub\\PictureADayAligner\\haarcascade_frontalface_default.xml')
-#eye_cascade = cv2.CascadeClassifier('C:\\Users\\hosfo\\Documents\\GitHub\\PictureADayAligner\\haarcascade_eye.xml')
-
-#failed_path = 'C:\\Users\\hosfo\\Documents\\GitHub\\PictureADayAligner\\FailedDetections'
-#img_name = 'test2Brandon.jpg'
-#img_path = 'C:\\Users\\hosfo\\Documents\\GitHub\\PictureADayAligner' + '\\' + img_name
-#directory = r'C:\Users\hosfo\Documents\GitHub\PictureADayAligner'
-
 def convertToVideo(finished_path):
+    print("")
+    print("Generating Video", end="")
     img_array = []
     for entry in os.scandir(finished_path):
         img = cv2.imread(entry.path)
@@ -31,12 +24,12 @@ def convertToVideo(finished_path):
     
     out = cv2.VideoWriter('PictureADay.mp4', 0x7634706d, 10.0, size)
     for i in range(len(img_array)):
+        print(".")
         out.write(img_array[i])
     out.release()
 
 def processFolder(directory_path, img_folder, make_video):
     print(directory_path)
-    #os.chdir(directory_path)
     #creating face_cascade and eye_cascade objects
     face_cascade = cv2.CascadeClassifier(directory_path + r'\haarcascade_frontalface_default.xml')
     eye_cascade = cv2.CascadeClassifier(directory_path + r'\haarcascade_eye.xml')
@@ -50,6 +43,10 @@ def processFolder(directory_path, img_folder, make_video):
     failed_path = directory_path + r'\FailedDetections'
     finished_path = directory_path + r'\FixedImages'
 
+    #Check if image folder is populated
+    if not os.listdir(imgs_path):
+        print("IMAGE FOLDER IS EMPTY")
+        return
     #iterate through all images in folder and fix
     for entry in os.scandir(imgs_path):
         img_name = entry.path.split(os.sep)[-1]
@@ -76,7 +73,7 @@ def straightenImage(img_name, img_path, directory, failed_path, face_cascade, ey
         cv2.imwrite(os.path.join(failed_path, img_name),img)
         return
 
-    # Defining and drawing* the rectangle around the face
+    # Defining the rectangle around the face
     x, y, w, h = faces[0]
 
     # Creating two regions of interest
@@ -116,10 +113,6 @@ def straightenImage(img_name, img_path, directory, failed_path, face_cascade, ey
     right_eye_x = right_eye_center[0]
     right_eye_y = right_eye_center[1]
     
-    #cv2.circle(roi_color, left_eye_center, 5, (255, 0, 0) , -1)
-    #cv2.circle(roi_color, right_eye_center, 5, (255, 0, 0) , -1)
-    #cv2.line(roi_color,right_eye_center, left_eye_center,(0,200,200),3)
-
     if left_eye_y > right_eye_y:
         A = (right_eye_x, left_eye_y)
         # Integer -1 indicates that the image will rotate in the clockwise direction
@@ -129,12 +122,6 @@ def straightenImage(img_name, img_path, directory, failed_path, face_cascade, ey
         # Integer 1 indicates that image will rotate in the counter clockwise direction 
         direction = 1 
 
-    #cv2.circle(roi_color, A, 5, (255, 0, 0) , -1)
-    
-    #cv2.line(roi_color,right_eye_center, left_eye_center,(0,200,200),3)
-    #cv2.line(roi_color,left_eye_center, A,(0,200,200),3)
-    #cv2.line(roi_color,right_eye_center, A,(0,200,200),3)
-
     delta_x = right_eye_x - left_eye_x
     delta_y = right_eye_y - left_eye_y
     angle=np.arctan(delta_y/delta_x)
@@ -142,26 +129,19 @@ def straightenImage(img_name, img_path, directory, failed_path, face_cascade, ey
 
     eyes_midpoint = ((left_eye_x + right_eye_x)//2, (left_eye_y + right_eye_y)//2)
 
-    
-    
-
     # Width and height of the image
     h, w = img.shape[:2]
     # Calculating a center point of the image
     center = (w // 2, h // 2)
 
-    # Defining a matrix M and calling
-    # cv2.getRotationMatrix2D method
+    # Defining a matrix M and calling cv2.getRotationMatrix2D method
     M = cv2.getRotationMatrix2D(center, (angle), 1.0)
     # Applying the rotation to our image 
     rotated = cv2.warpAffine(img, M, (w, h))
 
     # calculate distance between the eyes in the first image
-    dist_1 = np.sqrt((delta_x * delta_x) + (delta_y * delta_y))
 
     ratio = 1
-
-
     # Defining aspect ratio of a resized image
     dim = (int(w * ratio), int(h * ratio))
     # We have obtained a new image that we call resized3
@@ -183,7 +163,7 @@ def straightenImage(img_name, img_path, directory, failed_path, face_cascade, ey
 
 
     cv2.imwrite(os.path.join(finished_path, newFilename),translated_image)
-    print("Done")
+    print(newFilename +":","Done")
 
 
 
